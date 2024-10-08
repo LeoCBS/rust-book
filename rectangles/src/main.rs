8434d80c42db22948f76d5b5d2a630fe31f6a294
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2024-10-08 17:39:34 -0300

    finish struct section

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,8 +3,33 @@ fn main() {
     let width = 30;
     let heigth = 50;
     println!("the rectangle area is {}", area(width, heigth));
+    
+    let rect = (30,50);
+    println!("the rectangle area is {}", area_tuple(rect));
+   
+    let scale = 2;
+    let rect_struct = Rectangle{
+        width:dbg!(30 * scale),
+        heigth:50,
+    };
+    //println!("the rectangle area is {}", area_struct(rect_struct));
+    println!("the rectangle struct is {rect_struct:#?}");
 }
 
 fn area(width: u32, heigth:u32) -> u32{
     width*heigth
+}
+
+fn area_tuple(dimensions: (u32, u32)) -> u32{
+    dimensions.0 * dimensions.1
+}
+
+#[derive(Debug)]
+struct Rectangle{
+    width: u32,
+    heigth: u32,
+}
+
+fn area_struct(rect:Rectangle)-> u32{
+    rect.width * rect.heigth
 }

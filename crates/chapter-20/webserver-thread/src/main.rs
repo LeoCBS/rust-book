ec0a5f17702f82f7086b17c95d43370b99c766fd
Leonardo Cesar Borges <leonardo.borges@neoway.com.br>
2024-11-25 11:22:21 -0300

    finish chapter 20

--- crates/chapter-20/webserver-thread/src/main.rs
+++ crates/chapter-20/webserver-thread/src/main.rs
@@ -1,20 +1,26 @@
+mod lib;
+
+use lib::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
-    thread,
+    thread::{self},
     time::Duration,
 };
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        println!("connection established");
-        handler_connection(stream);
+        pool.execute(|| {
+            handler_connection(stream);
+        });
     }
+    println!("Shutting down.");
 }
 
 fn handler_connection(mut stream: TcpStream) {
